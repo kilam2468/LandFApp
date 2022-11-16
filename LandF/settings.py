44ad68a52db3899cc71b41980092b1e36facbe3e
Malik M <kilam2468@gmail.com>
2022-11-16 15:28:41 -0500

    staticfiles fix

--- LandF/settings.py
+++ LandF/settings.py
@@ -128,15 +128,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'staticfiles'
 ]
 
 MEDIA_ROOT = BASE_DIR / 'static/images/userprofileimages'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
